b420004e9bfcea9935a004f06b32f3ff35798351
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-04-21 12:34:11 -0500

    se contruyen por completo las entidades Producto,TipoHorario y TipoPrenda, con esta base esta proxima la construccion de todas las entidades

--- src/main/java/com/caciquesport/model/configTypes/TipoHorario.java
+++ src/main/java/com/caciquesport/model/configTypes/TipoHorario.java
@@ -1,13 +1,27 @@
 package com.caciquesport.model.configTypes;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+@Table(name="tipo_horario")
 public class TipoHorario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_horario")
+    private int idHorario;
 
-    private int id;
-
+    @Column(name = "horario",nullable = false,unique = true,length = 10,updatable = false)
     private String horario;
 
     
