66ccfeecf94c28b3cbfca4b9de1aa390a7484b75
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-04-21 22:14:58 -0500

    se crearon todas las clases, y se agregaron todas las anotaciones necesarias para que jpa trabaje con los atributos de cada clase

--- src/main/java/com/caciquesport/model/entity/Producto.java
+++ src/main/java/com/caciquesport/model/entity/Producto.java
@@ -1,8 +1,9 @@
 package com.caciquesport.model.entity;
 
+import com.caciquesport.model.configTypes.Institucion;
+import com.caciquesport.model.configTypes.Talla;
 import com.caciquesport.model.configTypes.TipoHorario;
 import com.caciquesport.model.configTypes.TipoPrenda;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,13 +17,28 @@ public class Producto {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ip_producto")
-    private int idProducto;
+    private int id;
     
-    @ManyToOne
+    @ManyToOne  
+    @JoinColumn(name = "tipo_prenda_id")
     private TipoPrenda tipoPrenda;
 
     @ManyToOne
+    @JoinColumn(name = "tipo_horario_id")
     private TipoHorario tipoHorario;
 
+    @ManyToOne
+    @JoinColumn(name = "talla_id")
+    private Talla talla;
 
+    @ManyToOne
+    @JoinColumn(name = "institucion_id")
+    private Institucion institucion;
+    
+    @OneToOne
+    @JoinColumn(name = "detalle_producto")
+    private DetalleProducto detalleProducto;
+
+
+    
 }
