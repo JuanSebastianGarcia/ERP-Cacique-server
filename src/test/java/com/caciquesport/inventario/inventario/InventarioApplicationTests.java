5f05efbe80e494acc1842f54649c05f84d164c92
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-04-22 19:18:31 -0500

    prueba #2 de conectividad con el repositorio remoto

--- src/test/java/com/caciquesport/inventario/inventario/InventarioApplicationTests.java
+++ src/test/java/com/caciquesport/inventario/inventario/InventarioApplicationTests.java
@@ -3,11 +3,15 @@ package com.caciquesport.inventario.inventario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.caciquesport.model.configTypes.Talla;
+
 @SpringBootTest
 class InventarioApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Talla talla=new Talla(1,"xl");
 	}
 
 }
