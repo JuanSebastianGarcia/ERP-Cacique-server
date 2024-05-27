4c89deb725f3a31fbf223df4177c761282e2b6fa
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-05-27 03:52:28 -0500

    este commit realiza cambios en la conexion a la base de datos a un nivel local

--- src/main/java/com/caciquesport/inventario/inventario/InventarioApplication.java
+++ src/main/java/com/caciquesport/inventario/inventario/InventarioApplication.java
@@ -2,13 +2,19 @@ package com.caciquesport.inventario.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class InventarioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventarioApplication.class, args);
+		//SpringApplication.run(InventarioApplication.class, args);
+		
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String passwordEncrypt=encoder.encode("admin");
+
+        System.out.println(passwordEncrypt);
 	}
 
 }
