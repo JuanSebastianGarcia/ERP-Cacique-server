382ac219bde311c87aaff5d32f01e1b7d8b37b2e
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-06-08 19:57:09 -0500

    se estan configurando aspectos de seguridad

--- src/main/java/com/caciquesport/inventario/inventario/config/CorsConfig.java
+++ src/main/java/com/caciquesport/inventario/inventario/config/CorsConfig.java
@@ -1,22 +1,45 @@
 package com.caciquesport.inventario.inventario.config;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /*
  * esta clase configura los cors de la aplicacion
  */
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig{
 
-    @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization");
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration corseConfig = new CorsConfiguration();
+
+        agregarReglas(corseConfig);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("**/", corseConfig);
+
+        return source;
     }
+
+
+    /*
+     * agrega las reglas requeridas a los cors
+     */
+    private void agregarReglas(CorsConfiguration corseConfig) {
+
+        corseConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corseConfig.setAllowedHeaders(Arrays.asList("GET","POST","PUT","DELETE"));
+        corseConfig.setAllowedHeaders(Arrays.asList("*"));
+    }
+
+
+
+
 }
--- src/main/java/com/caciquesport/inventario/inventario/config/SecurityConfig.java
+++ src/main/java/com/caciquesport/inventario/inventario/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.caciquesport.inventario.inventario.config;
 
 
+import java.beans.Customizer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,13 +22,20 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf
+                .disable())
+                
             .authorizeHttpRequests(customRequest -> customRequest
                 .requestMatchers(HttpMethod.GET,"/api/**").permitAll()
-                .requestMatchers(HttpMethod.PUT).denyAll())
-                
+                .requestMatchers(HttpMethod.PUT,"/api/**").permitAll()
+                .requestMatchers(HttpMethod.POST,"/api/**").permitAll()
+                .requestMatchers(HttpMethod.DELETE,"/api/**").permitAll())
+
+
+
             .sessionManagement(sess -> sess
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         
 
 
