15513d108db9445103b9f16a8b7f0ddf548ed815
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-10-05 11:08:14 -0500

    se termino de organizar el codigo que genera un pdf para cada factura generada, por lo que cada vez que se genere una nueva factura se generara el  pdf con su informacion

--- src/main/java/com/caciquesport/inventario/inventario/service/implementations/NotificacionFacturaImpl.java
+++ src/main/java/com/caciquesport/inventario/inventario/service/implementations/NotificacionFacturaImpl.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.caciquesport.inventario.inventario.model.entity.Factura;
+import com.caciquesport.inventario.inventario.model.entity.Producto;
 import com.caciquesport.inventario.inventario.service.interfaces.NotificacionFacturaService;
 
 import com.itextpdf.kernel.colors.*;
@@ -27,23 +28,28 @@ public class NotificacionFacturaImpl implements NotificacionFacturaService {
      * @param factura - factura la cual sera notificada
      */
     @Override
-    public void notificarFactura(Factura factura){
-        
-        //se verifica que el cliente tenga correo
-        if(!factura.getCliente().getEmail().isEmpty()){
+    public void notificarFactura(Factura factura) {
+
+        // se verifica que el cliente tenga correo
+        if (!factura.getCliente().getEmail().isEmpty()) {
             try {
                 generarPdf(factura);
             } catch (Exception e) {
-                //se agregara un mensaje que notifique que la factura no fue enviada al correo
+                // se agregara un mensaje que notifique que la factura no fue enviada al correo
             }
         }
 
-        
     }
 
-    public Document generarDocumentoParaPDf() throws FileNotFoundException{
+    /**
+     * Generar documento pdf en el cual se agregara todo el contenido para la
+     * factura
+     * 
+     * @return document - documento pdf
+     */
+    public Document generarDocumentoParaPDf() throws FileNotFoundException {
 
-        //ruta para el correo
+        // ruta para el correo
         String dest = System.getProperty("user.dir") + "/facturaspdf/" + "factura1.pdf";
 
         // Inicializar el PdfWriter
@@ -57,17 +63,74 @@ public class NotificacionFacturaImpl implements NotificacionFacturaService {
 
     public void generarPdf(Factura factura) throws FileNotFoundException {
 
-        //generar documento
-        Document document =generarDocumentoParaPDf();
+        // generar documento
+        Document document = generarDocumentoParaPDf();
 
         // Color personalizado
         Color greenColor = new DeviceRgb(139, 195, 74);
 
         construirTitulo(document);
 
-        //generar datos de la factura como codigo, titular, fecha
-        generarDetallesFactura(document,factura);
+        // generar datos de la factura como codigo, titular, fecha
+        generarDetallesFactura(document, factura);
 
+        // crear tabla y agregar productos
+        crearLlenarTabla(document, factura, greenColor);
+
+        //agregar Totales
+        document.add(new Paragraph("\nTotal: $"+factura.getSoportePago().getValorTotalFactura())
+                .setFontSize(15)
+                .setBold()
+                .setTextAlignment(TextAlignment.RIGHT));
+
+        double valorRestante=factura.getSoportePago().getValorTotalFactura()-factura.getSoportePago().getValorTotalPagado();
+        document.add(new Paragraph("Valor total cancelado: $"+factura.getSoportePago().getValorTotalPagado()+
+        "\nValor total adeudado: $"+valorRestante).setFontSize(15));
+
+
+        //agregar informacion final
+        agregarInformacionFinal(document,greenColor);
+        
+        // Cerrar el documento
+        document.close();
+    }
+
+    /**
+     * agregar datos e informacion final de la factura. estos datos son generales y no representan
+     * detalles relevantes para la factura como tal
+     * 
+     * @param document - instancia del documento pdf de la factura
+     */
+    private void agregarInformacionFinal(Document document, Color greenColor) {
+        
+        // Mensaje de agradecimiento
+        document.add(new Paragraph("Muchas gracas")
+                .setFontSize(16)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER)
+                .setFontColor(greenColor));
+
+        document.add(new Paragraph("Cacique Sport cuida la imagen de tu hijo en su etapa mas importante")
+                .setFontSize(13)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER)
+                .setFontColor(greenColor));
+
+        // Datos de contacto
+        document.add(new Paragraph("Cra. 26 #No. 46 - 31, Calarcá, Quindío\nTeléfono: 3233392630")
+                .setFontSize(10)
+                .setTextAlignment(TextAlignment.CENTER));
+
+    }
+
+    /**
+     * Generar una tabla para el documento y agregar la lista de productos
+     * 
+     * @param document   - instancia del documento pdf para la factura
+     * @param factura    - instancia de la factura con la informacion
+     * @param greenColor - color base para pintar la tabla
+     */
+    private void crearLlenarTabla(Document document, Factura factura, Color greenColor) {
 
         // Crear tabla de productos
         float[] columnWidths = { 4, 4, 4 }; // Anchos de las columnas
@@ -79,85 +142,69 @@ public class NotificacionFacturaImpl implements NotificacionFacturaService {
         table.addHeaderCell(new Cell().add(new Paragraph("Estado").setBold().setFontColor(greenColor)));
         table.addHeaderCell(new Cell().add(new Paragraph("Precio").setBold().setFontColor(greenColor)));
 
-        // Filas de la tabla
-        table.addCell("Camibuso");
-        table.addCell("Pendiente");
-        table.addCell("$50.000");
-
-        table.addCell("Pantalon");
-        table.addCell("Entregado");
-        table.addCell("$60.000");
-
-        table.addCell("Medias");
-        table.addCell("Entregado");
-        table.addCell("$12.000");
+        agregarProductosTabla(table, factura);
 
         document.add(table);
-
-        // Totales
-        document.add(new Paragraph("\nTotal: $122.000")
-                .setFontSize(15)
-                .setBold()
-                .setTextAlignment(TextAlignment.RIGHT));
-
-        document.add(new Paragraph("Valor total cancelado: $72.000\nValor total adeudado: $50.000").setFontSize(15));
-
-        // Mensaje de agradecimiento
-        document.add(new Paragraph("Muchas gracas")
-                .setFontSize(16)
-                .setBold()
-                .setTextAlignment(TextAlignment.CENTER)
-                .setFontColor(greenColor));
-
-            document.add(new Paragraph("Cacique Sport cuida la imagen de tu hijo en su etapa mas importante")
-                .setFontSize(13)
-                .setBold()
-                .setTextAlignment(TextAlignment.CENTER)
-                .setFontColor(greenColor));
-
-        // Datos de contacto
-        document.add(new Paragraph("Cra. 26 #No. 46 - 31, Calarcá, Quindío\nTeléfono: 3233392630")
-                .setFontSize(10)
-                .setTextAlignment(TextAlignment.CENTER));
-
-        // Cerrar el documento
-        document.close();
     }
 
+    /**
+     * Agregar la lista de productos de la factura a una tabla
+     * 
+     * @param table   - tabla que pertenece al documento pdf
+     * @param factura - instancia de la factura con la informacion
+     */
+    private void agregarProductosTabla(Table table, Factura factura) {
 
+        // agregar todos los productos de la factura
+        for (int i = 0; i < factura.getListaProductosFactura().size(); i++) {
+            Producto producto = factura.getListaProductosFactura().get(i).getProducto();
+
+            // se contruye la informacion para la columna de producto
+            String caracteristicasProducto = producto.getTipoPrenda().toString() + " del "
+                    + producto.getTipoInstitucion() + " " + producto.getTipoTalla() +
+                    " de " + producto.getTipoHorario() + "para " + producto.getTipoGenero();
+
+            String estado = factura.getListaProductosFactura().get(i).getEstadoProducto().toString();
+
+            String precio = "" + producto.getDetalleProducto().getPrecio();
+
+            // Filas de la tabla
+            table.addCell(caracteristicasProducto);
+            table.addCell(estado);
+            table.addCell(precio);
+        }
+
+    }
 
     /**
      * Este metodo agrega datos generales a la factura. los datos son:
      * codigo de la factura, titular de la factura, fecha de la factura y estado
      */
     private void generarDetallesFactura(Document document, Factura factura) {
-        
+
         // Detalles de la factura
         document.add(new Paragraph("Factura de venta")
                 .setFontSize(15)
                 .setBold());
 
-        String codigo=factura.getIdFactura().toString();
-        String fecha =factura.getFechaFactura().toString();
-        String titular =factura.getCliente().getNombre();
-        String estado =factura.getEstadoFactura().toString();
+        String codigo = factura.getIdFactura().toString();
+        String fecha = factura.getFechaFactura().toString();
+        String titular = factura.getCliente().getNombre();
+        String estado = factura.getEstadoFactura().toString();
 
-        document.add(new Paragraph("Código: "+codigo+"\n" +
-                "Fecha: "+fecha+"\n" +
-                "Titular: "+titular+"\n" +
-                "Estado: "+estado).setFontSize(13));
+        document.add(new Paragraph("Código: " + codigo + "\n" +
+                "Fecha: " + fecha + "\n" +
+                "Titular: " + titular + "\n" +
+                "Estado: " + estado).setFontSize(13));
     }
 
-
-    
     /**
-     * agregar un titulo principal  al documento
+     * agregar un titulo principal al documento
      * 
      * @param document - documento al que se le agrega el titulo
      */
     private void construirTitulo(Document document) {
-        
-                
+
         document.add(new Paragraph("Cacique Sport")
                 .setFontSize(40)
                 .setBold()
